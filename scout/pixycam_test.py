1634391b1486f005b44e13d420625be34dd30dae
Evan Martino <evan@Evans-MBP.local>
2020-02-06 18:55:56 -0500

    pixy updates

--- scout/pixycam_test.py
+++ scout/pixycam_test.py
@@ -3,6 +3,7 @@ import pixy
 from ctypes import *
 from pixy import *
 import time
+import math
 
 pixy.init()
 pixy.change_prog("color_connected_components")
@@ -10,6 +11,8 @@ pixy.change_prog("color_connected_components")
 radius = 10
 x_center = 158    #center of cam in x dir
 y_center = 104    #center of cam in y dir
+
+base_center_target = (158, 104)
 
 upper_x = x_center + radius
 lower_x = x_center - radius
@@ -28,32 +31,43 @@ class Blocks (Structure):
 
 blocks = BlockArray(100)
 
-while 1:
-  count_in_frame = pixy.ccc_get_blocks(100, blocks)
+def phase1():
+    complete = False
+    while not complete:
+        objects_seen = pixy.ccc_get_blocks(100, blocks)
+        
+        #TODO: add procedure for if there is no base detected
 
-  if count_in_frame > 0:
-    for i in range (0, count_in_frame):
-        #   print('[BLOCK: SIG=%d X=%3d Y=%3d WIDTH=%3d HEIGHT=%3d]' % (blocks[i].m_signature, blocks[i].m_x, blocks[i].m_y, blocks[i].m_width, blocks[i].m_height))
-        target_block = blocks[i] # temporarily choose the last block in the frame as the target
-
-    if target_block.m_x < lower_x:
-        # brightness = map(lower_x - target_block.m_x, 0, lower_x, 255, 0)
-        print("Left")
-
-    if target_block.m_x > upper_x:
-        # brightness = map(target_block.m_x - upper_x, 0, upper_x, 255, 100)
-        print("Right")
-
-    if target_block.m_y < upper_y:
-        # brightness = map(upper_y - target_block.m_y, 0, upper_y, 255, 100)
-        print("Up")
-
-    if target_block.m_y > lower_y:
-        # brightness = map(target_block.m_y - lower_y, 0, lower_y, 255, 100)
-        print("Down")
-
-    if target_block.m_x < upper_x and target_block.m_y > upper_y and target_block.m_x > lower_x and target_block.m_y < lower_y:
-    	print("Centered, lower.")
-    time.sleep(1)
+        if objects_seen > 0:
+            lowest_distance_to_center = 1000
+            target_block = None
+            # choose the most centered block in the frame
+            for i in range (0, objects_seen):
+                if blocks[i].m_signature == 0:
+                    #   print('[BLOCK: SIG=%d X=%3d Y=%3d WIDTH=%3d HEIGHT=%3d]' % (blocks[i].m_signature, blocks[i].m_x, blocks[i].m_y, blocks[i].m_width, blocks[i].m_height))
+                    distance_to_center = math.sqrt((blocks[i].m_x - x_center)**2 + (blocks[i].m_y - y_center)**2)  
+                    if distance_to_center < lowest_distance_to_center:
+                        lowest_distance_to_center = distance_to_center
+                        target_block = blocks[i] 
+            
+            if target_block:
+                movement_vector = (base_center_target[0] - target_block.m_x, base_center_target[1] - target_block.m_y)
+                print(movement_vector)
+            # if target_block.m_x < lower_x:
+            #     # brightness = map(lower_x - target_block.m_x, 0, lower_x, 255, 0)
+            #     print("Left")
+            # if target_block.m_x > upper_x:
+            #     # brightness = map(target_block.m_x - upper_x, 0, upper_x, 255, 100)
+            #     print("Right")
+            # if target_block.m_y < upper_y:
+            #     # brightness = map(upper_y - target_block.m_y, 0, upper_y, 255, 100)
+            #     print("Up")
+            # if target_block.m_y > lower_y:
+            #     # brightness = map(target_block.m_y - lower_y, 0, lower_y, 255, 100)
+            #     print("Down")
+            # if target_block.m_x < upper_x and target_block.m_y > upper_y and target_block.m_x > lower_x and target_block.m_y < lower_y:
+            #     print("Centered, lower.")
+            
+            time.sleep(1)
     
   
