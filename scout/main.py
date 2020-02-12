67d95c3b39b20abd0f98bb58498ce0cadaf9a912
Evan Martino <evan@Evans-MBP.local>
2020-02-12 16:39:18 -0500

    bug

--- scout/main.py
+++ scout/main.py
@@ -1,11 +1,8 @@
 import os,sys,inspect
-from utils.flight_utils import *
 import sys
 import multiprocessing
-current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir) 
 # import utils.bluetooth 
+from utils.flight_utils import *
 
 # vehicle = None
 connection_string = '/dev/serial0'
