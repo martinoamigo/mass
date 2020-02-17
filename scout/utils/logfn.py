53b380dd237d1dbffd329465c7f8f9c1e1171394
Giulio Cristello <50882312+gcristello@users.noreply.github.com>
2020-02-17 16:44:01 -0500

    Create logfn.py

---
+++ scout/utils/logfn.py
@@ -0,0 +1,13 @@
+import logging
+
+logging.basicConfig(
+    filename="masterCatalog.log",
+    filemode="a",
+    format=("%(asctime)s:%(levelname)s:%(message)s"),
+    level=logging.INFO
+)
+  
+
+class Log:
+  def __init__(self):
+    self.log = logging.getLogger()
