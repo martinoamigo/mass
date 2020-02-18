1d8add64fb4e67ac4ba6639b0466b36ce67f525b
Evan Martino <martinoman98@gmail.com>
2020-02-18 16:38:17 -0500

    nogpsguided hehehehehe

--- scout/utils/flight_utils.py
+++ scout/utils/flight_utils.py
@@ -119,7 +119,7 @@ def arm_and_takeoff(base, vehicle, aTargetAltitude):
 
     base.send("Arming motors")
     # Copter should arm in GUIDED mode
-    vehicle.mode = VehicleMode("STABILIZE")
+    vehicle.mode = VehicleMode("GUIDEDNOGPS")
     vehicle.armed = True
 
     while not vehicle.armed:      
