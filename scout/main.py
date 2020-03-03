6a66d124fce6b761f7ef9032e3fe7688591c50ee
Evan Martino <martinoman98@gmail.com>
2020-03-03 20:00:17 +0000

    spacing error

--- scout/main.py
+++ scout/main.py
@@ -38,27 +38,22 @@ class Blocks (Structure):
 blocks = pixy.BlockArray(100)
 
 def get_base_position():
-    objects_seen = pixy.ccc_get_blocks(100, blocks)
-        
-    #TODO: add procedure for if there is no base detected
-
-    if objects_seen > 0:
-        lowest_distance_to_center = 1000
-        target_block = None
-        # choose the most centered block in the frame
-        for i in range (0, objects_seen):
-            if blocks[i].m_signature == 1:
-                #   print('[BLOCK: SIG=%d X=%3d Y=%3d WIDTH=%3d HEIGHT=%3d]' % (blocks[i].m_signature, blocks[i].m_x, blocks[i].m_y, blocks[i].m_width, blocks[i].m_height))
-                distance_to_center = math.sqrt((blocks[i].m_x - x_center)**2 + (blocks[i].m_y - y_center)**2)  
-                if distance_to_center < lowest_distance_to_center:
-                    lowest_distance_to_center = distance_to_center
-                    target_block = blocks[i] 
-        
-        if target_block:
+	objects_seen = pixy.ccc_get_blocks(100, blocks)
+	if objects_seen > 0:
+		lowest_distance_to_center = 1000
+		target_block = None
+		# choose the most centered block in the frame
+		for i in range (0, objects_seen):
+			if blocks[i].m_signature == 1:
+				#   print('[BLOCK: SIG=%d X=%3d Y=%3d WIDTH=%3d HEIGHT=%3d]' % (blocks[i].m_signature, blocks[i].m_x, blocks[i].m_y, blocks[i].m_width, blocks[i].m_height))
+				distance_to_center = math.sqrt((blocks[i].m_x - x_center)**2 + (blocks[i].m_y - y_center)**2)  
+				if distance_to_center < lowest_distance_to_center:
+					lowest_distance_to_center = distance_to_center
+					target_block = blocks[i]
+		if target_block != None:
 			position_vector = (target_block.m_x, target_block.m_y)
-        	# position_vector = (target_block.m_x - base_center_target[0], base_center_target[1] - target_block.m_y)
-            return position_vector
-
+			# position_vector = (target_block.m_x - base_center_target[0], base_center_target[1] - target_block.m_y)		
+		return target_block
 
 def bluetooth_listener(base, vehicle):
 	""" Runs in the background to receive any messages sent from the base. Will only process one message at a time."""
